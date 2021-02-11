8e0229bf1160f8221de1048b71e48ee491b848a0
MaxReus11 <popov.maksim@phystech.edu>
2021-02-11 18:13:01 +0300

    Добавьте файлы проекта.

---
+++ dz1_2nd_try/Test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	cout << "Hello, world"<<endl;
+	cout << "First change"<<endl;
+	cout << "Some other changes";
+	
+	int a = 10, b = 1;
+
+	return 0;
+}
